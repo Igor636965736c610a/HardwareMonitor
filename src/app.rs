29a364373d227c61de3ce1268de02099ba59b697
Igor <igormiekina@gmail.com>
2023-11-09 00:50:38 +0100

    usings fix, network plots refactor

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,12 @@
 use std::collections::VecDeque;
-use egui::plot::{Line, Legend, PlotBounds};
-use egui::{containers::*, *};
-use sysinfo::{NetworkExt, NetworksExt,  System, SystemExt, CpuExt, DiskExt};
+use egui::{SidePanel, RichText, Color32, Layout, Align, plot, Grid};
+use egui::plot::{Line, Legend, PlotBounds, Plot};
+use sysinfo::{NetworkExt, NetworksExt,  System, SystemExt, CpuExt, MacAddr};
 use std::sync::Arc;
-use crate::app::mutex::Mutex;
+use std::sync::Mutex;
 use std::thread;
 use core::time::Duration;
 use egui::plot::Corner;
-use crate::app::plot::Plot;
 
 //#[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct ProcessManagerApp {
@@ -21,12 +20,11 @@ pub struct ProcessManagerAppMutexData{
     total_cpu_usage: i16,
     memory_usage: i16,
     swap_usage: i16,
+    network_informations: Vec<NetworkInformations>,
     cpu_performance_data_points: Data<f32>,
     cpus_performance_data_points: Vec<CpuData>,
     memory_usage_data_points: Data<f32>,
     swap_usage_data_points: Data<f32>,
-    transmitted_network_data_points: Data<f32>,
-    recived_network_data_points: Data<f32>,
     network_y_plot_bound: f64
 }
 
@@ -41,12 +39,26 @@ impl ProcessManagerApp {
         let host_name = system.host_name();
         let os_version = system.os_version();
         let kernel_version = system.kernel_version();
+        let network_informations = system.networks().iter()
+            .enumerate()
+            .map(|(i, x)|{
+                NetworkInformations {
+                    number: i + 1,
+                    interface_name: x.0.to_string(),
+                    mac_address: x.1.mac_address(),
+                    is_display_on_plot: true,
+                    recived_plot_points: None,
+                    transmitted_plot_points: None,
+                    recived_bytes: 0,
+                    transmitted_bytes: 0,
+                    total_errors_on_recived: 0,
+                    total_errors_on_transmitted: 0,
+                }
+            }).collect();
 
         let cpu_performance_data_points = Data::new(20);
         let memory_usage_data_points = Data::new(20);
         let swap_usage_data_points = Data::new(20);
-        let transmitted_network_data_points = Data::new(20);
-        let recived_network_data_points = Data::new(20);
         let cpus_performance_data_points: Vec<CpuData> = system.cpus().iter().map(|x|{
             CpuData { name: x.name().to_string(), usage: x.cpu_usage(), is_display_on_plot: false, plot_points: None }
         }).collect();
@@ -59,9 +71,8 @@ impl ProcessManagerApp {
             cpu_performance_data_points,
             memory_usage_data_points,
             swap_usage_data_points,
-            transmitted_network_data_points,
-            recived_network_data_points,
             cpus_performance_data_points,
+            network_informations,
             network_y_plot_bound: 100.0
         }));
 
@@ -84,7 +95,7 @@ impl ProcessManagerApp {
         thread::spawn(move || {
             loop {
                 {
-                    let process_manager_mutex_data = &mut *arc_process_manager_mutex_data.lock();
+                    let process_manager_mutex_data = &mut *arc_process_manager_mutex_data.lock().unwrap();
 
                     process_manager_mutex_data.system.refresh_cpu();
                     process_manager_mutex_data.system.refresh_memory();
@@ -130,48 +141,41 @@ impl ProcessManagerApp {
                     //     println!("in: {} xxxxxxx {} ----------- {} ------------ {}", network.received(), network.transmitted(), network.mac_address(), interface_name);
                     // }
 
-                    let network_main_interface = match process_manager_mutex_data.system.networks().iter().next(){
-                        Some(network_main_interface) => {
-                            //println!("in: {} xxxxxxx {} ----------- {} ------------ {}", 
-                            //    network_main_interface.1.received(), network_main_interface.1.transmitted(), network_main_interface.1.mac_address(), network_main_interface.0);
-
-                            network_main_interface
-                        }
-                        None => {
-                            panic!("xd");
-                        }
-                    };
-
                     // for cpu in process_manager_mutex_data.system.cpus() {
                     //     println!("{}%", cpu.cpu_usage());
                     // }
 
-                    println!("=> disks:");
-                    for disk in process_manager_mutex_data.system.disks() {
-                        println!("name {}", disk.name().to_string_lossy().to_string());
-                        println!("available_space {}", disk.available_space());
-                        let file_system = match std::str::from_utf8(disk.file_system()) {
-                            Ok(value) => {
-                                value
-                            }
-                            Err(_) => {
-                                panic!("nie panikuj!")
-                            }
-                        };
-                        println!("file_system {}", file_system);
-                        println!("is_removable {}", disk.is_removable());
-                        println!("mount_point {}", disk.mount_point().to_string_lossy().to_string());
-                        println!("total_space {}", disk.total_space());
-                        println!("kind {:?}", disk.kind());
-                        println!("");
+                    println!("=> networks:");
+                    for (interface_name, data) in process_manager_mutex_data.system.networks() {
+                        println!("{}: {}/{} B", data.mac_address(), data.received(), data.transmitted());
                     }
 
-                    println!("");
-                    println!("ORIGINAL");
-                    println!("=> disks:");
-                    for disk in process_manager_mutex_data.system.disks() {
-                        println!("{:?}", disk);
-                    }
+                    // println!("=> disks:");
+                    // for disk in process_manager_mutex_data.system.disks() {
+                    //     println!("name {}", disk.name().to_string_lossy().to_string());
+                    //     println!("available_space {}", disk.available_space());
+                    //     let file_system = match std::str::from_utf8(disk.file_system()) {
+                    //         Ok(value) => {
+                    //             value
+                    //         }
+                    //         Err(_) => {
+                    //             panic!("nie panikuj!")
+                    //         }
+                    //     };
+                    //     println!("file_system {}", file_system);
+                    //     println!("is_removable {}", disk.is_removable());
+                    //     println!("mount_point {}", disk.mount_point().to_string_lossy().to_string());
+                    //     println!("total_space {}", disk.total_space());
+                    //     println!("kind {:?}", disk.kind());
+                    //     println!("");
+                    // }
+
+                    // println!("");
+                    // println!("ORIGINAL");
+                    // println!("=> disks:");
+                    // for disk in process_manager_mutex_data.system.disks() {
+                    //     println!("{:?}", disk);
+                    // }
 
                     process_manager_mutex_data.system.cpus().iter().for_each(|x|{
                         let cpu_data = process_manager_mutex_data.cpus_performance_data_points.iter_mut().find(|y| y.name == x.name());
@@ -206,20 +210,72 @@ impl ProcessManagerApp {
                     process_manager_mutex_data.cpu_performance_data_points.push(processor);
                     process_manager_mutex_data.memory_usage_data_points.push(memory as f32);
                     process_manager_mutex_data.swap_usage_data_points.push(swap as f32);
-                    process_manager_mutex_data.transmitted_network_data_points.push(network_main_interface.1.transmitted() as f32);
-                    process_manager_mutex_data.recived_network_data_points.push(network_main_interface.1.received() as f32);
+                    process_manager_mutex_data.network_informations.iter_mut().for_each(|x| {
+                        let net_data = process_manager_mutex_data.system.networks().iter()
+                            .find(|y| y.1.mac_address().eq(&x.mac_address));
+                        match net_data {
+                            Some(data) => {
+                                if x.is_display_on_plot {
+                                    x.recived_bytes = data.1.received();
+                                    x.transmitted_bytes = data.1.transmitted();
+                                    match &mut x.recived_plot_points {
+                                        Some(points) => {
+                                            points.push(data.1.received());
+                                        }
+                                        None => {
+                                            let mut plot_points: Data<u64> = Data::new(20);
+                                            plot_points.push(data.1.received());
+                                            x.recived_plot_points = Some(plot_points);
+                                        }
+                                    }
+                                    match &mut x.transmitted_plot_points {
+                                        Some(points) => {
+                                            points.push(data.1.transmitted());
+                                        }
+                                        None => {
+                                            let mut plot_points: Data<u64> = Data::new(20);
+                                            plot_points.push(data.1.transmitted());
+                                            x.transmitted_plot_points = Some(plot_points);
+                                        }
+                                    }
+                                }
+                                else {
+                                    x.recived_plot_points = None;
+                                    x.transmitted_plot_points = None;
+                                }
+                            }
+                            None => {
+                                panic!("nie panikuj!");
+                            }
+                        }    
+                    });
 
-                    let recived_max = process_manager_mutex_data.recived_network_data_points.data_iter()
-                        .max_by(|a, b| a.total_cmp(b)).unwrap_or(&100.0).max(100.0);
-                    let transmitted_max = process_manager_mutex_data.transmitted_network_data_points.data_iter()
-                        .max_by(|a, b| a.total_cmp(b)).unwrap_or(&100.0).max(100.0);
-                    
-                    process_manager_mutex_data.network_y_plot_bound = if recived_max > transmitted_max {
-                        recived_max as f64
-                    } else {
-                        transmitted_max as f64
+                    let mut net_y_bound: u64 = 100;
+    
+                    for info in &process_manager_mutex_data.network_informations {
+                        if let Some(data) = &info.recived_plot_points {
+                            if info.is_display_on_plot {
+                                for value in &data.data_records {
+                                    if *value > net_y_bound {
+                                        net_y_bound = *value;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    for info in &process_manager_mutex_data.network_informations {
+                        if let Some(data) = &info.transmitted_plot_points {
+                            if info.is_display_on_plot {
+                                for value in &data.data_records {
+                                    if *value > net_y_bound {
+                                        net_y_bound = *value;
+                                    }
+                                }
+                            }
+                        }
                     }
                     
+                    process_manager_mutex_data.network_y_plot_bound = net_y_bound as f64;              
                 }
             
                 //println!("TEST1SEKUNDA");
@@ -239,7 +295,7 @@ impl eframe::App for ProcessManagerApp {
         let window_size = _frame.info().window_info.size;
 
         let mutex_data_arc = Arc::clone(&self.process_manager_mutex_data);
-        let mut mutex_data = mutex_data_arc.lock();
+        let mut mutex_data = mutex_data_arc.lock().unwrap();
 
         let cpu_points: Vec<[f64; 2]> = {
             mutex_data.cpu_performance_data_points.data_iter().enumerate().map(|(index, &i)| {
@@ -259,17 +315,17 @@ impl eframe::App for ProcessManagerApp {
             }).collect()
         };
 
-        let transmitted_network_points: Vec<[f64; 2]> = {
-            mutex_data.transmitted_network_data_points.data_iter().enumerate().map(|(index, &i)| {
-                [index as f64, f64::from(i)]
-            }).collect()
-        };
+        // let transmitted_network_points: Vec<[f64; 2]> = {
+        //     mutex_data.transmitted_network_data_points.data_iter().enumerate().map(|(index, &i)| {
+        //         [index as f64, f64::from(i)]
+        //     }).collect()
+        // };
 
-        let recived_network_points: Vec<[f64; 2]> = {
-            mutex_data.recived_network_data_points.data_iter().enumerate().map(|(index, &i)| {
-                [index as f64, f64::from(i)]
-            }).collect()
-        };
+        // let recived_network_points: Vec<[f64; 2]> = {
+        //     mutex_data.recived_network_data_points.data_iter().enumerate().map(|(index, &i)| {
+        //         [index as f64, f64::from(i)]
+        //     }).collect()
+        // };
         
         let plot_bounds = PlotBounds::from_min_max([0.0, 0.0], [20.0, 100.0]);
         let mut max_y_network_plot_bound = mutex_data.network_y_plot_bound;
@@ -366,15 +422,48 @@ impl eframe::App for ProcessManagerApp {
                     .legend(Legend::default().position(Corner::RightTop).background_alpha(0.0))
                     .reset();
 
-                let network_transmitted_line = Line::name(Line::new(transmitted_network_points), "bytes transmitted");
-                let network_recived_line = Line::name(Line::new(recived_network_points), "bytes recived");
+                // let network_transmitted_line = Line::name(Line::new(transmitted_network_points), "bytes transmitted");
+                // let network_recived_line = Line::name(Line::new(recived_network_points), "bytes recived");
 
 
                 network_plot.show(ui, |plot_ui: &mut plot::PlotUi|{
-                    plot_ui.line(network_transmitted_line);
-                    plot_ui.line(network_recived_line);
+                    mutex_data.network_informations.iter().for_each(|x| {
+                        match &x.transmitted_plot_points {
+                            Some(points) => {
+                                let transmitted_line: Vec<[f64; 2]> = {
+                                    points.data_iter().enumerate().map(|(index, &i)| {
+                                        [index as f64, i as f64]
+                                    }).collect()
+                                };
+                                let name = format!("{}. bytes transmitted", x.number);
+                                plot_ui.line(Line::new(transmitted_line).name(name));
+                            }
+                            None => {
+                            }                         
+                        }
+                        match &x.recived_plot_points {
+                            Some(points) => {
+                                let recived_line: Vec<[f64; 2]> = {
+                                    points.data_iter().enumerate().map(|(index, &i)| {
+                                        [index as f64, i as f64]
+                                    }).collect()
+                                };
+                                let name = format!("{}. bytes recived", x.number);
+                                plot_ui.line(Line::new(recived_line).name(name));
+                            }
+                            None => {
+                            } 
+                        }
+                    });
                     plot_ui.set_plot_bounds(PlotBounds::from_min_max([0.0, 0.0], [20.0, max_y_network_plot_bound]));
                 })
+            });
+
+            ui.menu_button("networks", |inner_ui|{
+                mutex_data.network_informations.iter_mut().for_each(|net|{
+                    let text = format!("{}. {}{}{}", net.number, net.interface_name, "\n", net.mac_address);
+                    inner_ui.checkbox(&mut net.is_display_on_plot, text);
+                });
             });
         });
 
@@ -454,4 +543,27 @@ struct CpuData{
     usage: f32,
     is_display_on_plot: bool,
     plot_points: Option<Data<f32>>
+}
+
+struct DiskInformations{
+    name: String,
+    available_space: u64,
+    file_system: String,
+    is_removable: String,
+    mount_point: String,
+    total_space: u64,
+    kind: String,
+}
+
+struct NetworkInformations {
+    number: usize,
+    interface_name: String,
+    mac_address: MacAddr,
+    is_display_on_plot: bool,
+    recived_plot_points: Option<Data<u64>>,
+    transmitted_plot_points: Option<Data<u64>>,
+    recived_bytes: u64,
+    transmitted_bytes: u64,
+    total_errors_on_recived: u64,
+    total_errors_on_transmitted: u64,
 }
